60f898eeee54c5eb62bde0c008afa2844e761075
Hamlet Valladares <sigorahv@gmail.com>
2021-08-17 16:25:20 -0400

    initial commit, got react app working

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>HAMLETS RESUME</h1>
+      <p>
+      HAMLETS RESUME
+      </p>
+    </div>
+  );
+}
