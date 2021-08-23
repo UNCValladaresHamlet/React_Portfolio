29688deba173ed61c5fd2432bf9ed2f44b734d25
Hamlet Valladares <sigorahv@gmail.com>
2021-08-23 19:22:21 -0400

    added data to my resume and cleaned up code

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol, MDBBtn} from 'mdb-react-ui-kit';
+import Project_1 from '../../img/portfolio/Project_1.png'
+import Project_2 from '../../img/portfolio/Project_2.PNG'
+import password from '../../img/portfolio/password.png'
+import schedule from '../../img/portfolio/schedule.png'
+import note_taker from '../../img/portfolio/note_taker.PNG'
+import fitness_tracker from '../../img/portfolio/fitness_tracker.PNG'
+
+
+import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol} from 'mdb-react-ui-kit';
 
 export default function Portfolio() {
   return (
+    
     <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
       <MDBCol>
 
-        <MDBCard>
+        <MDBCard class= ".card-img-top" alignment='center'>
           <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/041.jpg'
+            src= {Project_1}
             alt='...'
             position='top'
           />
           <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardTitle>Inspired Meal Prep </MDBCardTitle>
             <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
+            Our goal was to create a website that provides users a way to get a personalized meal plan for the week based on the user's daily calorie goal, diet, and any food they would want to exclude. Also, they can save their favorite recipes on a separate page for later. The website should suggest an easy to digest meal plan that would make it simple to get a curated list of recipes that fit the users preferences. Every day the user gets a new motivational quote to inspire them.
+
             </MDBCardText>
 
             <div className="text-center">
-            <MDBBtn>Primary</MDBBtn>
+            <a href="https://anthonyplanisek.github.io/Project-1/">Inspired Meal Prep Link</a>
             </div>
 
           </MDBCardBody>
@@ -28,21 +37,20 @@ export default function Portfolio() {
       </MDBCol>
       <MDBCol>
 
-        <MDBCard>
+        <MDBCard class= ".card-img-top" alignment='center'>
           <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/042.jpg'
+            src= {Project_2}
             alt='...'
             position='top'
           />
           <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardTitle>Traveler's Landing</MDBCardTitle>
             <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
+            This is a website generated for North Carolina travelers on a budget. When the traveler enters what location they would like to travel to the site will provide museums, parks, trails, and restaurants in the area. The traveler will be able choose if they would like to view trails, parks, museums, or restaurants. The traveler will be provided with the address, cost, hours, and name of the place which they can save to their profile. The traveler must create a profile and login to search.
             </MDBCardText>
 
             <div className="text-center">
-            <MDBBtn>Primary</MDBBtn>
+            <a href="https://dream-vacation2021.herokuapp.com/">Traveler's Landing Link</a>
             </div>
 
           </MDBCardBody>
@@ -50,21 +58,21 @@ export default function Portfolio() {
       </MDBCol>
 
       <MDBCol>
-        <MDBCard>
+      <MDBCard class= ".card-img-top" alignment='center'>
           <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/043.jpg'
+            src= {password}
             alt='...'
             position='top'
           />
           <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardTitle>Password Generator</MDBCardTitle>
             <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
+            This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
             </MDBCardText>
 
             <div className="text-center">
-            <MDBBtn>Primary</MDBBtn>
+            <a href="https://uncvalladareshamlet.github.io/Password_Generator/">Password Generator Link</a>
             </div>
 
           </MDBCardBody>
@@ -72,21 +80,20 @@ export default function Portfolio() {
       </MDBCol>
 
       <MDBCol>
-        <MDBCard>
+      <MDBCard class= ".card-img-top" alignment='center'>
           <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/044.jpg'
+            src= {schedule}
             alt='...'
             position='top'
           />
           <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardTitle>Work Day Scheduler</MDBCardTitle>
             <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
+            Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
             </MDBCardText>
 
             <div className="text-center">
-            <MDBBtn>Primary</MDBBtn>
+            <a href="https://uncvalladareshamlet.github.io/HW_5_WorkDayScheduler/">Workday Scheduler Link</a>
             </div>
 
           </MDBCardBody>
@@ -94,21 +101,20 @@ export default function Portfolio() {
       </MDBCol>
 
       <MDBCol>
-        <MDBCard>
+      <MDBCard class= ".card-img-top" alignment='center'>
           <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/041.jpg'
+            src= {note_taker}
             alt='...'
             position='top'
           />
           <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardTitle>Note Taker</MDBCardTitle>
             <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
+            An app called Note Taker that can be used to write, save, and delete notes. This application uses an Express.js back end and saves and retrieves note data from a JSON file. Users are also able to delete notes as they need as well.
             </MDBCardText>
 
             <div className="text-center">
-            <MDBBtn>Primary</MDBBtn>
+            <a href="https://note-taker-app44.herokuapp.com/">Note Taker Link</a>
             </div>
             
           </MDBCardBody>
@@ -116,21 +122,20 @@ export default function Portfolio() {
       </MDBCol>
 
       <MDBCol>
-        <MDBCard>
+      <MDBCard class= ".card-img-top" alignment='center'>
           <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/041.jpg'
+            src= {fitness_tracker}
             alt='...'
             position='top'
           />
           <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardTitle>Fitness Tracker</MDBCardTitle>
             <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
+            An app called Fitness Tracker that can be used to view, create, and track daily workouts. This app enables you to log multiple exercises in a workout on a given day. It also can track the name, type, weight, sets, reps, and duration of your exercises. 
             </MDBCardText>
 
             <div className="text-center">
-            <MDBBtn>Primary</MDBBtn>
+            <a href="https://fast-fjord-59620.herokuapp.com/">Fitness Tracker Link</a>
             </div>
 
           </MDBCardBody>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import { FaHtml5, FaCss3Alt, FaReact, FaNode, FaDatabase, FaNpm, FaBootstrap, FaGit, FaGithub} from "react-icons/fa";
+import {IoLogoJavascript} from "react-icons/io"
 
 export default function Blog() {
   return (
-    <div>
-      <h1>HAMLETS RESUME</h1>
-      <p>
-      HAMLETS RESUME
-      </p>
+    <div class="container my-class">
+            <h1>Programming Languages & Tools</h1>
+                  <ul class="list-inline">
+                    <li class="list-inline-item"> <FaHtml5 size={70} /> </li>
+                    <li class="list-inline-item"> <FaCss3Alt size={70}/> </li>
+                    <li class="list-inline-item"> <IoLogoJavascript size={70}/> </li>
+                    <li class="list-inline-item"> <FaReact size={70}/> </li>
+                    <li class="list-inline-item"> <FaNode size={70}/> </li>
+                    <li class="list-inline-item"> <FaDatabase size={70}/> </li>
+                    <li class="list-inline-item"> <FaNpm size={70}/> </li>
+                    <li class="list-inline-item"> <FaBootstrap size={70}/> </li>
+                    <li class="list-inline-item"> <FaGit size={70}/> </li>
+                    <li class="list-inline-item"> <FaGithub size={70}/> </li>
+                  </ul>
     </div>
+    
   );
 }
