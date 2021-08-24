1459719035445d2ed1dcba80da7354bf1851f1a5
Hamlet Valladares <sigorahv@gmail.com>
2021-08-24 02:33:25 -0400

    updated node modules

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,24 @@
-import React from 'react';
-
+import React from "react";
+// import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-Form Goes Here
-      </p>
-    </div>
+<MDBContainer>
+  <MDBRow>
+    <MDBCol md="6">
+      <form>
+        <p className="h5 text-center mb-4">Sign in</p>
+        <div className="grey-text">
+          <MDBInput label="Type your email" icon="envelope" group type="email" validate error="wrong"
+            success="right" />
+          <MDBInput label="Type your password" icon="lock" group type="password" validate />
+        </div>
+        <div className="text-center">
+          <MDBBtn>Login</MDBBtn>
+        </div>
+      </form>
+    </MDBCol>
+  </MDBRow>
+</MDBContainer>
   );
 }
